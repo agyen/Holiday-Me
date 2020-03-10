a1330c6c7207bff3739e32eb827feed44e4cc80f
bill-n <teebill15@gmail.com>
2020-03-10 18:04:48 +0000

    made oidc redirect env variable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,31 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { OpenidService } from '../service/openid.service';
-import { ActivatedRoute } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
+import { Component, OnInit } from "@angular/core";
+import { OpenidService } from "../service/openid.service";
+import { ActivatedRoute } from "@angular/router";
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
   authenticationCode;
   idToken;
   isValid = false;
-  oidc_redirect_path: any;
+  oidc_redirect_path = "https://google.com";
 
-   redirectToOIDCPage = this.oidc_redirect_path;
-
-  constructor(private openId: OpenidService, 
-              private activatedRoute: ActivatedRoute,
-              private cookieservice: CookieService) { 
-                this.oidc_redirect_path = this.cookieservice.get("oidc_redirect_url")
-              }
+  constructor(
+    private openId: OpenidService,
+    private activatedRoute: ActivatedRoute,
+    private cookieservice: CookieService
+  ) {}
+  // redirectToOIDCPage = this.oidc_redirect_path;
 
   ngOnInit() {
     this.activatedRoute.queryParamMap.subscribe(queryParam => {
       this.authenticationCode = queryParam.get("code");
-      this.openId.postAuthenticationCodForAccessAndIdToken(this.authenticationCode).subscribe()
+      this.openId
+        .postAuthenticationCodForAccessAndIdToken(this.authenticationCode)
+        .subscribe();
     });
-  } 
+    this.oidc_redirect_path = this.cookieservice.get("oidc_redirect_url");
+  }
 }
