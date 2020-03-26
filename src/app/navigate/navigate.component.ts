2094e26fd733ab52622feb7fdb8d1464212cbc63
bill-n <teebill15@gmail.com>
2020-03-26 16:27:55 +0000

    added url path

--- src/app/navigate/navigate.component.ts
+++ src/app/navigate/navigate.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
-import { OpenIdService } from '../service/openId.service';
-import { CookieService } from 'ngx-cookie-service/cookie-service/cookie.service';
-
+import { OpenIdService } from "../service/openId.service";
+import { CookieService } from "ngx-cookie-service/cookie-service/cookie.service";
 
 @Component({
   selector: "app-navigate",
@@ -10,27 +9,24 @@ import { CookieService } from 'ngx-cookie-service/cookie-service/cookie.service'
   styleUrls: ["./navigate.component.css"]
 })
 export class NavigateComponent implements OnInit {
-
   authenticationCode;
   isValid = false;
   oidc_redirect_path;
-  
 
-
-
-  constructor( private openId: OpenIdService,
+  constructor(
+    private openId: OpenIdService,
     private activatedRoute: ActivatedRoute,
-    private router: Router) {}
+    private router: Router
+  ) {}
 
   ngOnInit() {
-      this.activatedRoute.queryParamMap.subscribe(queryParam => {
+    this.activatedRoute.queryParamMap.subscribe(queryParam => {
       this.authenticationCode = queryParam.get("code");
       this.oidc_redirect_path = this.openId.redirect_page_to_oidc;
       this.router.navigate(["/"]).then(result => {
-      window.location.href =
-      this.oidc_redirect_path});
+        window.location.href =
+          "https://accounts.google.com/o/oauth2/v2/auth?scope=openid%20email&access_type=offline&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=https://holiday-request.herokuapp.com/requester&response_type=code&client_id=859455735473-bgmqqco3q588kgaog0g2k0fmnur5qvf9.apps.googleusercontent.com&hd=turntabl.io&prompt=consent";
+      });
     });
-    
   }
-
 }
